8575bde03551726714245a28caf53038bda28c11
Ryan Huffaker <ryanjayhuffaker@gmail.com>
2016-04-19 12:11:21 -0600

    Installed angular-animate, implemented in card-modal-module

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,8 +1,8 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
-//import ngAnimate from 'angular-animate';
+import ngAnimate from 'angular-animate';
 
 angular.module('freedomsworn', [
-  angularMeteor, uiRouter
+  angularMeteor, uiRouter, ngAnimate
 ]);
